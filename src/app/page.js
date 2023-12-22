a8963f2c8351b0865c6a171058dba0d3abeda661
Douglas Vidal <vidaldl01@gmail.com>
2023-12-21 22:14:32 -0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="mt-0">
-
+      
       <div className="flex flex-col items-center justify-center max-w-2xl w-full mx-auto m-3 p-3">
         <Image
           src="/spider.png"
@@ -13,12 +13,10 @@ export default function Home() {
           className='rounded-full shadow-md'
         />
 
-        <h1 className='mt-3 mb-3 shadow-sm'>
-          Spider Man
-        </h1>
+        <h1 className='mt-3 shadow-sm'>Spider Man</h1>
       </div>
 
-      <div className="flex items-center justify-center max-w-2xl h-screen">
+      <div className="flex items-center justify-center max-w-2xl w-full mx-auto h-screen m-3 p-3">
         <h1>Teste</h1>
       </div>
     </main>
