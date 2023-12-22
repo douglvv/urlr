55761f2326c058c212354e319673a7aeb0a70bc0
Douglas Vidal <vidaldl01@gmail.com>
2023-12-22 14:05:08 -0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="mt-0">
 
+      {/* Component Profile -> passar texto pelas props */}
       <div className="flex flex-col items-center justify-center max-w-2xl w-full mx-auto m-3 p-3">
         <Image
           src="/spider.png"
@@ -18,6 +19,7 @@ export default function Home() {
         <h4 className='shadow-sm text-gray-400 text-center text-sm sm:text-base'>
           Irure ullamco pariatur esse dolore ullamco dolore anim cupidatat officia sunt pariatur.
         </h4>
+
       </div>
 
       <div className="flex items-center justify-center max-w-2xl w-full mx-auto h-screen m-3 p-3">
