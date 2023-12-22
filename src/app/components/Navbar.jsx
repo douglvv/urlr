a42b94f95aaf335c131e1220c7e3c75374cda298
Douglas Vidal <vidaldl01@gmail.com>
2023-12-21 21:56:00 -0300

    update navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
 
     useEffect(() => {
         const handleScroll = () => {
-            if (window.scrollY > 100) {
+            if (window.scrollY > 80) {
                 setIsScrolled(true);
             } else {
                 setIsScrolled(false);
@@ -24,26 +24,26 @@ export default function Navbar() {
 
 
     return (
-        <header className='sticky z-50 top-0 transition duration-300'>
-            <nav className="flex items-center justify-center py-2 lg:py-4 transition duration-300">
+        <header className='sticky z-50 top-0'>
+            <nav className="flex items-center justify-center py-2 lg:py-4">
                 <div className={
                     isScrolled ?
-                        "flex w-full max-w-2xl items-center justify-between p-3 m-3 rounded-3xl border border-solid backdrop-blur border-gray-600 bg-opacity-50 bg shadow-sm transition"
+                        "flex w-full max-w-2xl items-center justify-between p-3 m-3 rounded-3xl border border-solid backdrop-blur border-gray-600 bg-opacity-50 bg shadow-sm"
                         :
-                        "flex w-full max-w-2xl items-center justify-between p-3 m-3 transition "
+                        "flex w-full max-w-2xl items-center justify-between p-3 m-3"
                 }
                 >
                     <div>
                         <a
                             className="my-1 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 lg:mb-0 lg:mt-0"
-                            href="#"
+                            href="/"
+                            title='Home'
                         >
                             <Image
                                 src="/logo.png"
                                 width={33}
                                 height={33}
                                 alt="Logo"
-                                title='Home'
                             />
                         </a>
                     </div>
@@ -61,7 +61,7 @@ export default function Navbar() {
                     )}
 
                     <div className='flex items-center justify-center'>
-                        <button className='text-2xl'><FcAbout /></button>
+                        <button className='text-2xl' title='Sobre'><FcAbout /></button>
                     </div>
                 </div>
             </nav>
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,25 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <main className="mt-0">
-      <div className="flex items-center justify-center h-screen">
-          <h1>Teste</h1>
+
+      <div className="flex flex-col items-center justify-center max-w-2xl w-full mx-auto m-3 p-3">
+        <Image
+          src="/spider.png"
+          width={96}
+          height={96}
+          alt="Profile Pic"
+          className='rounded-full shadow-md'
+        />
+
+        <h1 className='mt-3 mb-3 shadow-sm'>
+          Spider Man
+        </h1>
       </div>
-      <div className="flex items-center justify-center h-screen">
-          <h1>Teste</h1>
+
+      <div className="flex items-center justify-center max-w-2xl h-screen">
+        <h1>Teste</h1>
       </div>
     </main>
   )
