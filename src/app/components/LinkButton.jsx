6b790161c36e8618a3d293f699946a2774f5dfbb
Douglas Vidal <vidaldl01@gmail.com>
2023-12-26 15:47:50 -0300

    add drag icon on LinkButton component

--- src/app/components/LinkButton.jsx
+++ src/app/components/LinkButton.jsx
@@ -1,17 +1,19 @@
 import Link from 'next/link'
+import { RxDragHandleHorizontal } from 'react-icons/rx';
 
-export default function LinkButton({link}) {
-    return (
-        <div className="flex items-center justify-center">
-            <Link
-                className="w-full max-w-xl mx-3 mt-3 mb-2 p-3 rounded-lg
+export default function LinkButton({ link }) {
+  return (
+    <div className="flex items-center justify-center">
+      <Link
+        className="flex items-center w-full max-w-xl mx-3 mt-3 mb-2 p-3 rounded-lg
                         border border-[#6C757D] shadow-sm shadow-[#343A40]
-                        font-semibold text-base text-center block text-[#ADB5BD]"
-                href={link.url}
-                target='blank'
-            >
-                {link.title}
-            </Link>
-        </div>
-    )
+                        font-semibold text-base text-center text-[#ADB5BD]"
+        href={link.url}
+        target='_blank'
+      >
+        <RxDragHandleHorizontal title='Drag to reorder' className="mr-2 text-2xl" />
+        <span className="flex-1">{link.title}</span>
+      </Link>
+    </div>
+  )
 }
