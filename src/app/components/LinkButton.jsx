bb827c3c2c6690f369f350b134118395414dbc93
Douglas Vidal <vidaldl01@gmail.com>
2023-12-29 14:13:28 -0300

    reorganizacao dos componentes

--- src/app/components/LinkButton.jsx
+++ src/app/components/LinkButton.jsx
@@ -1,25 +1,43 @@
+"use client"
 import { RxDragHandleHorizontal } from 'react-icons/rx';
+import { Draggable } from 'react-beautiful-dnd';
+import { motion } from 'framer-motion';
 
-export default function LinkButton({ link, provided }) {
+export default function LinkButton({ link, index }) {
   return (
-    <a
-      className="w-full max-w-2xl mx-3 mt-3 mb-2 p-3 rounded-lg
+    <Draggable index={index} draggableId={link.url}>
+      {(provided) => (
+        <motion.li
+          className="flex"
+          drag
+          initial={{ x: '-100vw' }}
+          animate={{ x: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ type: 'spring', stiffness: 50, delay: index * 0.2 + 0.75 }}
+        >
+          <a
+            className="w-full max-w-2xl mx-3 mt-3 mb-2 p-3 rounded-lg
                         border border-[#6C757D] shadow-sm shadow-[#343A40]
                         font-semibold text-base text-center text-[#ADB5BD]
                         flex"
-      href={link.url}
-      target='_blank'
-      {...provided.draggableProps}
-      ref={provided.innerRef}
-    >
-      <div {...provided.dragHandleProps}>
-        <RxDragHandleHorizontal title='Drag to reorder' className="mr-2 text-2xl" />
-      </div>
+            href={link.url}
+            target='_blank'
+            {...provided.draggableProps}
+            ref={provided.innerRef}
+          >
+            <div {...provided.dragHandleProps}>
+              <RxDragHandleHorizontal title='Drag to reorder' className="mr-2 text-2xl" />
+            </div>
 
-      <div className="flex-1">
-        <span>{link.title}</span>
-      </div>
+            <div className="flex-1">
+              <span>{link.title}</span>
+            </div>
 
-    </a>
+          </a>
+        </motion.li>
+      )}
+    </Draggable>
   )
 }
+
+
--- src/app/components/Links.jsx
+++ src/app/components/Links.jsx
@@ -28,32 +28,7 @@ export default function Links() {
                     <ul className="list-none" ref={provided.innerRef} {...provided.droppableProps}>
                         <AnimatePresence>
                             {links.map((link, index) => (
-                                <Draggable key={link.url} index={index} draggableId={link.url}>
-                                    {(provided) => (
-                                        <motion.li
-                                            className="flex"
-                                            initial={{ x: '-100vw' }}
-                                            animate={{ x: 0 }}
-                                            drag // mudar para o icone de drag do componente
-                                            whileHover={{
-                                                scale: 1.05,
-                                                transition: { duration: .1 },
-
-                                            }}
-                                            whileDrag={{ scale: 1.1 }}
-                                            dragTransition={{ bounceStiffness: 100, bounceDamping: 0 }}
-                                            
-                                            whileTap={{ scale: 0.9 }}
-                                            whileInView={{ opacity: 1 }}
-                                            transition={{ type: 'spring', stiffness: 50, delay: index * 0.2 + 0.75 }}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                            ref={provided.innerRef}
-                                        >
-                                            <LinkButton link={link} provided={provided} />
-                                        </motion.li>
-                                    )}
-                                </Draggable>
+                                <LinkButton key={link.url} link={link} index={index} />
                             ))}
                         </AnimatePresence>
                         {provided.placeholder}
