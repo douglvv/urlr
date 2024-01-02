095052130d0e72213750107c7eab66125dc1fa8f
Douglas Vidal <vidaldl01@gmail.com>
2024-01-02 18:10:13 -0300

    fix

--- src/app/loading.js
+++ src/app/loading.js
@@ -2,7 +2,7 @@ import { FaLink } from 'react-icons/fa';
 
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center min-h-screen">
       <FaLink className="text-5xl animate-spin" />
     </div>
   );
