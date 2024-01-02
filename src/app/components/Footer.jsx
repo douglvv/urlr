53b5f71355fae23a09ba61616bc7104eb8ba5014
Douglas Vidal <vidaldl01@gmail.com>
2024-01-02 16:04:19 -0300

    update

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
         <footer
-            class="bg-[#1c1c1c] text-center text-sm sm:text-base lg:text-left mt-4">
-            <div class="p-4 text-center text-[#F8F9FA]">
+            className="bg-[#1c1c1c] text-center text-sm sm:text-base lg:text-left mt-4">
+            <div className="p-4 text-center text-[#F8F9FA]">
                 © 2023 Copyright:
                 <a
                     className="text-blue-500"
