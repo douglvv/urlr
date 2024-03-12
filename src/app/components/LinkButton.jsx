a3a913b3b5738578fbdf6f9180b45bd5e18e0b7a
Douglas Vidal <vidaldl01@gmail.com>
2024-03-12 17:20:56 -0300

    fix text alignment link buttons

--- src/app/components/LinkButton.jsx
+++ src/app/components/LinkButton.jsx
@@ -1,19 +1,23 @@
-"use client"
-import { RxDragHandleHorizontal } from 'react-icons/rx';
-import { Draggable } from 'react-beautiful-dnd';
-import { motion } from 'framer-motion';
+"use client";
+import { RxDragHandleHorizontal } from "react-icons/rx";
+import { Draggable } from "react-beautiful-dnd";
+import { motion } from "framer-motion";
 
 export default function LinkButton({ link, index }) {
   return (
     <Draggable index={index} draggableId={link.url}>
       {(provided) => (
         <motion.li
-          className="flex justify-center"          
+          className="flex justify-center"
           drag
           draggable
-          initial={{ x: '-100vw' }}
+          initial={{ x: "-100vw" }}
           animate={{ x: 0 }}
-          transition={{ type: 'spring', stiffness: 50, delay: index * 0.2 + 0.75 }}
+          transition={{
+            type: "spring",
+            stiffness: 50,
+            delay: index * 0.2 + 0.75,
+          }}
         >
           <div
             className="w-full max-w-xl my-3 mx-5 rounded-lg
@@ -25,25 +29,24 @@ export default function LinkButton({ link, index }) {
           >
             <div
               {...provided.dragHandleProps}
-              className="mr-2 p-3 text-2xl text-gray-400 transition duration-300
-              hover:text-[#F8F9FA] hover:scale-125 active:text-[#F8F9FA] active:scale-125"
+              className="p-3 text-2xl text-gray-400 transition duration-300
+              hover:text-[#F8F9FA] hover:scale-125 active:text-[#F8F9FA] 
+              active:scale-125 z-10"
             >
-              <RxDragHandleHorizontal title='Arraste para reordenar' />
+              <RxDragHandleHorizontal title="Arraste para reordenar" />
             </div>
 
-            <a className="flex-1 p-3"
+            <a
+              className="flex-1 p-3 -ml-12"
               href={link.url}
-              target='_blank'
+              target="_blank"
               title={link.title}
             >
               {link.title}
             </a>
-
           </div>
         </motion.li>
       )}
     </Draggable>
-  )
+  );
 }
-
-
